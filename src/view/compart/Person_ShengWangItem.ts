151b296fabb2937a1da71be143af913b4bbead9d
liyanmeng <799855844@qq.com>
2019-08-02 18:00:04 +0800

    updata jueSe panel_shengWang

--- src/view/compart/Person_ShengWangItem.ts
+++ src/view/compart/Person_ShengWangItem.ts
@@ -3,6 +3,13 @@ module view.compart{
 	export class Person_ShengWangItem extends ui.compart.Person_ShengWangItemUI{
 		constructor(){
 			super();
+			this.setData();
 		}
+			public setData(): void {
+			this.panel_shengWang.hScrollBarSkin = '';
+			this.hbox_shengWang['sortItem'] = (items) => { };
+
+		}
+
 	}
 }
