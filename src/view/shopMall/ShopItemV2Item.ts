43e301534f1efcbb36682be2e0d04aaab27be964
liyanmeng <799855844@qq.com>
2019-10-29 11:36:28 +0800

    商城

--- src/view/shopMall/ShopItemV2Item.ts
+++ src/view/shopMall/ShopItemV2Item.ts
@@ -7,8 +7,20 @@ module view.shopMall {
 		public item;
 		public setData(item: ProtoCmd.itf_Shop_RefreshResult): void {
 			this.item = item;
+			//折扣
+			this.img_zheKou.visible = false;
+			if (item.discount > 0) {
+				this.img_zheKou.visible = true;
+				this.img_zheKou.skin ='image/common/img_'+item.discount+'zhe.png' ;
+			}
+			//限购
+			this.lbl_xiangou.visible = false;
+			if (item.limitcnt > 0) {
+				this.lbl_xiangou.visible = true;
+				this.lbl_xiangou.text='限购'+item.limitcnt+'次' ;
+			}
 			//物品
-			let itemInfo = new ProtoCmd.ItemBase;
+			let itemInfo = new ProtoCmd.ItemBase();
 			itemInfo.dwBaseID = item.itemid;
 			itemInfo.dwCount = item.num;
 			itemInfo.dwBinding = item.binding;
@@ -19,58 +31,41 @@ module view.shopMall {
 			//物品价格
 			this.lbl_price.text = '' + item.price;
 			//物品货币类型
-			switch (item.pricetype) {
-				case EnumData.CoinType.COIN_TYPE_YUANBAO:
-					this.img_huobi.skin = 'image/main/icon_coin_' +item.pricetype+ '.png';
-					break;
-				case EnumData.CoinType.COIN_TYPE_YUANBAOLOCK:
-					this.img_huobi.skin =  'image/main/icon_coin_' +item.pricetype+ '.png';
-					break;
-				case EnumData.CoinType.COIN_TYPE_GOLD:
-					this.img_huobi.skin =  'image/main/icon_coin_' +item.pricetype+ '.png';
-					break;
-				case EnumData.CoinType.COIN_TYPE_HONOR:
-					this.img_huobi.skin = 'image/main/icon_coin_' + item.pricetype+'.png';
-					break;
-				case EnumData.CoinType.COIN_TYPE_GUILDSORCE:
-					this.img_huobi.skin =  'image/main/icon_coin_' +item.pricetype+ '.png';
-					break;
-
-			}
-			this.addEvent(item.itemid);
+			this.img_huobi.skin = LangConfig.getCoinImagePicSkin(item.pricetype);
+			this.addEvent();
 		}
-		public addEvent(id): void {
-			// this.on(Laya.UIEvent.CLICK, this, () => {
-			// 	let itemType = SheetConfig.mydb_item_base_tbl.getInstance(null).ITEMTYPE('' + id);
-			// 	let itemInfoDialog: view.dialog.ShopBuyItemV0Dialog | view.dialog.ShopBuyItemV1Dialog;
-			// 	// 根据物品类型显示不同界面
-			// 	switch (itemType) {
-			// 		// 材料
-			// 		case EnumData.ItemTypeDef.ITEM_TYPE_NORMAL:
-			// 			itemInfoDialog = new view.dialog.ShopBuyItemV0Dialog();
-			// 			break;
-			// 		// 装备
-			// 		case EnumData.ItemTypeDef.ITEM_TYPE_EQUIP:
-			// 			itemInfoDialog = new view.dialog.ShopBuyItemV1Dialog();
-			// 			break;
-			// 		// 消耗品
-			// 		case EnumData.ItemTypeDef.ITEM_TYPE_DRUG:
-			// 		case EnumData.ItemTypeDef.ITEM_TYPE_SKILL:
-			// 		case EnumData.ItemTypeDef.ITEM_TYPE_MAZE:
-			// 		case EnumData.ItemTypeDef.ITEM_TYPE_SCROLL:
-			// 			itemInfoDialog = new view.dialog.ShopBuyItemV0Dialog();
-			// 			break;
-			// 		// 任务物品
-			// 		case EnumData.ItemTypeDef.ITEM_TYPE_TASK:
-			// 			itemInfoDialog = new view.dialog.ShopBuyItemV0Dialog();
-			// 			break;
-			// 		// 默认
-			// 		default:
-			// 			itemInfoDialog = new view.dialog.ShopBuyItemV0Dialog();
-			// 			break;
-			// 	}
-			// 	itemInfoDialog.setData(this.item, EnumData.ShopBuyPanelType.SHOP_BUY_HOT_PANEL).show(true);
-			// })
+		public addEvent(): void {
+			this.on(Laya.UIEvent.CLICK, this, () => {
+				let itemType = SheetConfig.mydb_item_base_tbl.getInstance(null).ITEMTYPE('' + this.item.itemid);
+				let itemInfoDialog: view.shopMall.ShopBuyItemV0Dialog | view.shopMall.ShopBuyItemV1Dialog;
+				// 根据物品类型显示不同界面
+				switch (itemType) {
+					// 材料
+					case EnumData.ItemTypeDef.ITEM_TYPE_NORMAL:
+						itemInfoDialog = new view.shopMall.ShopBuyItemV0Dialog();
+						break;
+					// 装备
+					case EnumData.ItemTypeDef.ITEM_TYPE_EQUIP:
+						itemInfoDialog = new view.shopMall.ShopBuyItemV1Dialog();
+						break;
+					// 消耗品
+					case EnumData.ItemTypeDef.ITEM_TYPE_DRUG:
+					case EnumData.ItemTypeDef.ITEM_TYPE_SKILL:
+					case EnumData.ItemTypeDef.ITEM_TYPE_MAZE:
+					case EnumData.ItemTypeDef.ITEM_TYPE_SCROLL:
+						itemInfoDialog = new view.shopMall.ShopBuyItemV0Dialog();
+						break;
+					// 任务物品
+					case EnumData.ItemTypeDef.ITEM_TYPE_TASK:
+						itemInfoDialog = new view.shopMall.ShopBuyItemV0Dialog();
+						break;
+					// 默认
+					default:
+						itemInfoDialog = new view.shopMall.ShopBuyItemV0Dialog();
+						break;
+				}
+				itemInfoDialog.setData(this.item, EnumData.ShopBuyPanelType.SHOP_BUY_GUILD_PANEL).show(true);
+			})
 		}
 	}
 }
