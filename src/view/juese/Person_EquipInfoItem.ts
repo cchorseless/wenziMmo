8bc5d7f369a6839a9a4b1d7202a2c36d869a9223
liyanmeng <799855844@qq.com>
2019-11-15 15:37:09 +0800

    微调

--- src/view/juese/Person_EquipInfoItem.ts
+++ src/view/juese/Person_EquipInfoItem.ts
@@ -30,12 +30,16 @@ module view.juese {
 				o.setData(1)
 				o.popup();
 			})
-
+			//战斗属性介绍
+			EventManage.onWithEffect(this.btn_shuxing, Laya.UIEvent.CLICK, this, () => {
+				new view.dialog.InfoV1Dialog().popup();
+			})
 		}
 
 		public initUI(): void {
+			let func = LangConfig.getBigNumberDes;
 			//战力
-			this.lbl_zhanli.value = '' + LangConfig.getBigNumberDes(GameApp.MainPlayer.ability.nFight);
+			this.lbl_zhanli.value = '' + func(GameApp.MainPlayer.ability.nFight);
 			//玩家半身像
 			this.img_avatarPic.skin = LangConfig.getPlayerAvatarHalfSkin()
 			let allKey = Object.keys(GameApp.GameEngine.equipDB);
@@ -53,41 +57,41 @@ module view.juese {
 			let player = GameApp.MainPlayer;
 			let ability = player.ability;
 			// 血-生命值
-			this.lbl_Hp.text = '' + ability.nowHP + '/' + ability.nMaxHP;
+			this.lbl_Hp.text = '' + func(ability.nowHP) + '/' + func(ability.nMaxHP);
 			// 气-魔法值
-			this.lbl_Mp.text = '' + ability.nowMP + '/' + ability.nMaxMP;
+			this.lbl_Mp.text = '' + func(ability.nowMP) + '/' + func(ability.nMaxMP);
 			// 耐-内功值
-			this.lbl_neiGong.text = '' + ability.nowInnerValue + '/' + ability.nInnerValue;
+			this.lbl_neiGong.text = '' + func(ability.nowInnerValue) + '/' + func(ability.nInnerValue);
 			// 攻-攻击
 			switch (player.job) {
 				case EnumData.JOB_TYPE.JOB_WARRIOR:
-					this.lbl_atk.text = '' + ability.nMinDC + '-' + ability.nMaxDC;
+					this.lbl_atk.text = '' + func(ability.nMinDC) + '-' + func(ability.nMaxDC);
 					break;
 				case EnumData.JOB_TYPE.JOB_MAGE:
-					this.lbl_atk.text = '' + ability.nMinMC + '-' + ability.nMaxMC;
+					this.lbl_atk.text = '' + func(ability.nMinMC) + '-' + func(ability.nMaxMC);
 					break;
 				case EnumData.JOB_TYPE.JOB_MONK:
-					this.lbl_atk.text = '' + ability.nMinSC + '-' + ability.nMaxSC;
+					this.lbl_atk.text = '' + func(ability.nMinSC) + '-' + func(ability.nMaxSC);
 					break;
 			}
 			// 抗-物理防御
-			this.lbl_phyDef.text = '' + ability.nMinAC + '-' + ability.nMaxAC;
+			this.lbl_phyDef.text = '' + func(ability.nMinAC) + '-' + func(ability.nMaxAC);
 			// 化-魔法防御
-			this.lbl_migDef.text = '' + ability.nMinMAC + '-' + ability.nMaxMAC;
+			this.lbl_migDef.text = '' + func(ability.nMinMAC) + '-' + func(ability.nMaxMAC);
 			// 准-准确
-			this.lbl_zhunQue.text = '' + ability.nHit;
+			this.lbl_zhunQue.text = '' + func(ability.nHit);
 			// 躲-闪避
-			this.lbl_shanbi.text = '' + ability.nJuck;
+			this.lbl_shanbi.text = '' + func(ability.nJuck);
 			// 巧-暴击
-			this.lbl_baoJi.text = '' + ability.nCrit;
+			this.lbl_baoJi.text = '' + func(ability.nCrit);
 			// 狠-爆伤
-			this.lbl_baoShang.text = '' + ability.nAtkCrit;
+			this.lbl_baoShang.text = '' + func(ability.nAtkCrit);
 			// 幸-幸运
-			this.lbl_xingYun.text = '' + ability.nLucky;
+			this.lbl_xingYun.text = '' + func(ability.nLucky);
 			// 韧-韧性
-			this.lbl_renxing.text = '' + ability.nCritResi;
+			this.lbl_renxing.text = '' + func(ability.nCritResi);
 			// 侠义值-PK值
-			this.lbl_pk.text = '' + player.feature.getValue('wNowKilling');
+			this.lbl_pk.text = '' + func(player.feature.getValue('wNowKilling'));
 		}
 
 		/**
@@ -106,7 +110,7 @@ module view.juese {
 		public getEquipBackground(): void {
 			for (let i = 0; i < 10; i++) {
 				this['ui_item' + i].img_bg.visible = true;
-				this['ui_item' + i].img_bg.skin = 'image/common/daoju/itemicon_bg_' + (i + 10)+ '.png';
+				this['ui_item' + i].img_bg.skin = 'image/common/daoju/itemicon_bg_' + (i + 10) + '.png';
 			}
 		}
 	}
