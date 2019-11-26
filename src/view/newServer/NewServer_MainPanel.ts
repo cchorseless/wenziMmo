7c21bc9b4c452a87decd90b9f479e739df485638
liyanmeng <799855844@qq.com>
2019-11-26 15:50:39 +0800

    新服活动

--- src/view/newServer/NewServer_MainPanel.ts
+++ src/view/newServer/NewServer_MainPanel.ts
@@ -11,6 +11,7 @@ module view.newServer {
 			super();
 		}
 		public data = null;
+		public newServerActive = null;
 		public setData(data): NewServer_MainPanel {
 			this.data = data;
 			this.panel_newServer.hScrollBarSkin = '';
@@ -27,7 +28,7 @@ module view.newServer {
 		public init_newServer(): void {
 			let keys = Object.keys(this.data.General)
 			let name = [];
-			let dataArray=[];
+			let dataArray = [];
 			for (let key of keys) {
 				//活动名称不为零&&活动状态为1时显示
 				if (this.data.General[key].name != undefined && this.data.General[key].state == 1) {
@@ -38,6 +39,7 @@ module view.newServer {
 					this.view_newServer.addItem(box);
 				}
 			}
+			this.newServerActive = dataArray;
 			this.tab_top.labels = '' + name;
 			this.init_addBox();
 		}
@@ -45,27 +47,24 @@ module view.newServer {
 			this.box_time.visible = false
 			let index = this.tab_top.selectedIndex;
 			let box = this.view_newServer.getChildAt(index)
-			let ids = index + 1;
-			let id = this.data.General[ids].id;
 			let ui_newServer;
 			if (box.numChildren == 0) {
-				if (this.data.General[ids].state == 1) {
-					if (this.data != null) {
-						switch (id) {
-							case 10:
-								ui_newServer = view.newServer.NewServer_sportsItem;
-								break;
-							case 2:
-								ui_newServer = view.newServer.NewServer_allBossItem;
-								break;
-							case 3:
-								ui_newServer = view.newServer.NewServer_DragonItem;
-								break;
-						}
+				if (this.newServerActive != null) {
+					switch (this.newServerActive[index].id) {
+						case 10:
+							ui_newServer = view.newServer.NewServer_sportsItem;
+							break;
+						case 2:
+							ui_newServer = view.newServer.NewServer_allBossItem;
+							break;
+						case 3:
+							ui_newServer = view.newServer.NewServer_DragonItem;
+							break;
 					}
+					box.addChild(new ui_newServer());
 				}
 			}
-
+			this.view_newServer.selectedIndex=index;
 		}
 		public init_time(time: string): void {
 			this.box_time.visible = true;
