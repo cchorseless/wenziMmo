2f3f3c34125589c9bbe2ac285b2bba2ba3f4d55a
ThomasYgz <519939537@qq.com>
2019-11-27 10:57:51 +0800

    累计登录

--- src/view/newServer/NewServer_TotalLogin_Item.ts
+++ src/view/newServer/NewServer_TotalLogin_Item.ts
@@ -4,33 +4,47 @@ module view.newServer {
 		public id;
 		public data;
 		public desc;
+		public status;
 		constructor() {
 			super();
 			this.addEvent()
+			this.panel_item.hScrollBarSkin = '';
 		}
 		public addEvent() {
-			this.on(Laya.UIEvent.CLICK,this,function(){
+			this.on(Laya.UIEvent.CLICK, this, function () {
 				NewServer_TotalLoginItem.self.onChooseItem(this.id);
 			})
-			// this.img_box_State.on(Laya.UIEvent.CLICK,this,function(){
-			// 	let o = new view.activity.Active_Luck_Gift_Dialog()
-			// 	o.setData(this.data.items,this.desc)
-			// 	o.show();
-			// })
+			this.btn_get.on(Laya.UIEvent.CLICK, this, function () {
+				let pkt = new ProtoCmd.QuestClientData().setString(ProtoCmd.leijidenglu_minbandakai, [this.id])
+				lcp.send(pkt);
+			})
 		}
 		public setData(data) {
 			this.data = data;
 			this.id = data.day;
 			this.desc = data.desc;
-			this.lab_title.text = data.day + "天";
-			// if(data.status == 0){
-			// 	this.img_box_State.skin = "image/common/icon_baoxiang4_close.png"
-			// }else if(data.status == 1){
-			// 	this.img_box_State.skin = "image/common/icon_baoxiang4_light.png"
-			// }else if(data.status == 2){
-			// 	this.img_box_State.skin = "image/common/icon_baoxiang4_open.png"
-			// 	this.box_hasGet.visible = true;
-			// }
+			this.lab_title.text = this.desc;
+			this.status = data.status;
+			if (this.status == 1) {
+				this.btn_get.disabled = false;
+				this.btn_get.label = "领取"
+			} else if (this.status == 2) {
+				this.btn_get.disabled = true;
+				this.btn_get.label = "已领取"
+			} else if (this.status == 0) {
+				this.btn_get.disabled = true;
+				this.btn_get.label = "领取"
+			}
+			for (let i in data.items) {
+				let o = new compart.DaoJuItem();
+				let item = new ProtoCmd.ItemBase();
+				item.dwBaseID = data.items[i].index;
+				item.dwBinding = data.items[i].binding;
+				item.dwCount = data.items[i].num;
+				o.setData(item, EnumData.ItemInfoModel.SHOW_IN_MAIL)
+				o.x = (parseInt(i) - 1) * (o.width + 10)
+				this.hbox_item.addChild(o);
+			}
 		}
 	}
 }
