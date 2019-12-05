848da6e538cb50fc902e9ed84070cba0ab96281f
liyanmeng <799855844@qq.com>
2019-12-05 11:24:02 +0800

    主界面

--- src/view/juese/Person_MainPanel.ts
+++ src/view/juese/Person_MainPanel.ts
@@ -4,5 +4,50 @@ module view.juese{
 		constructor(){
 			super();
 		}
+		public setData(): void {
+			this.btn_self.selected = true;
+			this.tab_player.selectHandler = Laya.Handler.create(this, (index) => {
+				(this.viw_player.getChildAt(index) as any).setData();
+				this.viw_player.selectedIndex = index;
+			}, null, false);
+			this.addEvent();
+			this.init_Dizi();
+
+		}
+		public addEvent(): void {
+			EventManage.onWithEffect(this.btn_back, Laya.UIEvent.CLICK, this, () => {
+				PanelManage.openMainPanel()
+			})
+			EventManage.onWithEffect(this.btn_changeMode, Laya.UIEvent.CLICK, this, () => {
+				PanelManage.openJuQingModePanel()
+			})
+			// for (let job = 1; job < 4; job++) {
+			// 	EventManage.onWithEffect(this['btn_dizi' + job], Laya.UIEvent.CLICK, this, () => {
+			// 		PanelManage.openDiZiPanel(job);
+			// 	})
+			// }
+
+		}
+		//弟子基本信息发协议
+		public init_Dizi(): void {
+			let pkt = new ProtoCmd.QuestClientData();
+			pkt.setString(ProtoCmd.Hero_HeroBaseInfo, null, null, this, (jsonData: { [v: string]: ProtoCmd.itf_Hero_BaseInfo }) => {
+				// 更新弟子状态
+				for (let i = 1; i < 4; i++) {
+					switch (jsonData[i].JOB) {
+						case EnumData.JOB_TYPE.JOB_WARRIOR:
+							GameApp.MainPlayer.hero1.lockState = jsonData[i].STATE;
+							break;
+						case EnumData.JOB_TYPE.JOB_MAGE:
+							GameApp.MainPlayer.hero2.lockState = jsonData[i].STATE;
+							break;
+						case EnumData.JOB_TYPE.JOB_MONK:
+							GameApp.MainPlayer.hero3.lockState = jsonData[i].STATE;
+							break;
+					}
+				}
+			});
+			lcp.send(pkt);
+		}
 	}
 }
