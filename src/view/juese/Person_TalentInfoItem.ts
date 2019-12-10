e35b3d8ac88a800516d6c03c4eed80dcccdd6f37
liyanmeng <799855844@qq.com>
2019-12-10 10:54:22 +0800

    天赋

--- src/view/juese/Person_TalentInfoItem.ts
+++ src/view/juese/Person_TalentInfoItem.ts
@@ -49,16 +49,16 @@ module view.juese {
 			this.img_xiaoguo0.visible = true;
 			this.addEvent();
 			this.TalentInfo();
-
+			this.init_changeIcon();
 		}
 		public addEvent(): void {
+			//天赋切换
 			for (let i = 0; i < 5; i++) {
 				this['btn_top' + i].label = this.talentName[i]
 				this['btn_top' + i].on(Laya.UIEvent.CLICK, this, () => {
 					this.init_Initialization(i);
 				});
 			}
-
 			// 激活
 			this.btn_jiHuo.on(Laya.UIEvent.CLICK, this, () => {
 				let pkt = new ProtoCmd.QuestClientData();
@@ -67,7 +67,6 @@ module view.juese {
 				})
 				lcp.send(pkt);
 			})
-
 			// 升级
 			this.btn_up.on(Laya.UIEvent.CLICK, this, () => {
 				this.lvUpTalent();
@@ -75,9 +74,43 @@ module view.juese {
 			this.addLcpEvent();
 		}
 		/**
+    * 天赋按钮icon
+    */
+		public init_changeIcon(): void {
+			//初始化等级
+			for (let i = 0; i < 5; i++) {
+				this['lbl_lvl' + i].text = '未解锁';
+			}
+			let talentAllData = GameApp.MainPlayer.talentInfo;
+			for (let pos = EnumData.emEquipPosition.EQUIP_MEDAL; pos < EnumData.emEquipPosition.EQUIP_HERO_DRAGONHEART; pos++) {
+				let data = GameUtil.findEquipInPlayer(pos);
+				if (pos == EnumData.emEquipPosition.EQUIP_DRAGONSOUL && data) {
+					this.img_talent0.skin = 'image/common/daoju/itemicon_' + data.dwBaseID + '.png';
+					this.lbl_lvl0.text = 'LV.' + talentAllData[1];
+				}
+				if (pos == EnumData.emEquipPosition.EQUIP_SHIELD && data) {
+					this.img_talent1.skin = 'image/common/daoju/itemicon_' + data.dwBaseID + '.png';
+					this.lbl_lvl1.text = 'LV.' + talentAllData[3];
+				}
+				if (pos == EnumData.emEquipPosition.EQUIP_OFFICIALSEAL && data) {
+					this.img_talent2.skin = 'image/common/daoju/itemicon_' + data.dwBaseID + '.png';
+					this.lbl_lvl2.text = 'LV.' + talentAllData[5];
+				}
+				if (pos == EnumData.emEquipPosition.EQUIP_BLOODJADE && data) {
+					this.img_talent3.skin = 'image/common/daoju/itemicon_' + data.dwBaseID + '.png';
+					this.lbl_lvl3.text = 'LV.' + talentAllData[4];
+				}
+				if (pos == EnumData.emEquipPosition.EQUIP_MEDAL && data) {
+					this.img_talent4.skin = 'image/common/daoju/itemicon_' + data.dwBaseID + '.png';
+					this.lbl_lvl4.text = 'LV.' + talentAllData[2];
+				}
+			}
+		}
+		/**
 		 * 控制参数变化
 		 */
 		public init_Initialization(i): void {
+			//选中状态
 			for (let j = 0; j < 5; j++) {
 				this['btn_top' + j].selected = false;
 				this['img_xiaoguo' + j].visible = false;
@@ -86,45 +119,36 @@ module view.juese {
 			this['btn_top' + i].selected = true;
 			this['img_xiaoguo' + i].visible = true;
 			this.title = this.talentName[i];
-			let talentAllData = GameApp.MainPlayer.talentInfo;
 			switch (i) {
 				//悟性
 				case 0:
 					this.type = EnumData.emEquipPosition.EQUIP_DRAGONSOUL;
 					this.dangqianNum = EnumData.emTalentType.talent_wuXing;
-					this.img_type.skin = 'image/common/daoju/itemicon_123001.png';
-					this.lbl_lvl0.text = 'LV.' + talentAllData[1];
 					break;
 				//臂力
 				case 1:
 					this.type = EnumData.emEquipPosition.EQUIP_SHIELD;
 					this.dangqianNum = EnumData.emTalentType.talent_biLi;
-					this.img_type.skin = 'image/common/daoju/itemicon_122001.png';
-					this.lbl_lvl1.text = 'LV.' + talentAllData[3];
 					break;
 				//善缘
 				case 2:
 					this.type = EnumData.emEquipPosition.EQUIP_OFFICIALSEAL;
 					this.dangqianNum = EnumData.emTalentType.talent_shanYuan;
-					this.img_type.skin = 'image/common/daoju/itemicon_124001.png';
-					this.lbl_lvl2.text = 'LV.' + talentAllData[5];
 					break;
 				//身法
 				case 3:
 					this.type = EnumData.emEquipPosition.EQUIP_BLOODJADE;
 					this.dangqianNum = EnumData.emTalentType.talent_shenFa;
-					this.img_type.skin = 'image/common/daoju/itemicon_121001.png';
-					this.lbl_lvl3.text = 'LV.' + talentAllData[4];
 					break;
 
 				//根骨
 				case 4:
 					this.type = EnumData.emEquipPosition.EQUIP_MEDAL;
 					this.dangqianNum = EnumData.emTalentType.talent_genGu;
-					this.img_type.skin = 'image/common/daoju/itemicon_160001.png';
-					this.lbl_lvl4.text = 'LV.' + talentAllData[2];
 					break;
 			}
+			//拉取天赋协议
+			this.talent = this.eventList[i];
 			//升级天赋协议
 			this.upLevelType = this.UpEventList[i];
 			//激活天赋协议
@@ -132,9 +156,9 @@ module view.juese {
 			this.TalentInfo();
 		}
 		/**
-		 * 刷新天赋界面
-		 * 
-		 */
+	  * 刷新天赋界面
+	  * 
+	  */
 		public TalentInfo(): void {
 			if (this.getItemInfo()) {
 				this.viw_0.selectedIndex = 0;
@@ -153,11 +177,12 @@ module view.juese {
 			let id = this.client_func_index + 1000;
 			this.lbl_condition.text = '' + SheetConfig.Introduction_play.getInstance(null).TEXT1('' + id)
 			this.lbl_type.text = '先天' + this.title + '资质';
+			this.lbl_add.text = this.lbl_battle.text = '';
+			this.img_add.visible = this.img_battle.visible = false;
 		}
 		public getItemInfo(): ProtoCmd.ItemBase {
 			return GameUtil.findEquipInPlayer(this.type)
 		}
-
 		public addLcpEvent(): void {
 			// 拉取天賦
 			for (let event of this.eventList) {
@@ -165,8 +190,13 @@ module view.juese {
 					//所需经验
 					this.lbl_need.text = '' + jsonData.score;
 					//当前经验
+					if(jsonData.curscore<jsonData.score){
+						this.lbl_have.color='#a53232';
+					}else{
+						this.lbl_have.color='#000000';
+					}
 					this.lbl_have.text = '' + jsonData.curscore
-					let keys = Object.keys(jsonData.itemtab);
+					// let keys = Object.keys(jsonData.itemtab);
 					//升级天赋所需物品
 					// this.hbox_wupin.removeChildren();
 					// for (let key of keys) {
@@ -183,39 +213,15 @@ module view.juese {
 				})
 			}
 		}
-		public destroy(isbool): void {
-			for (let event of this.eventList) {
-				GameApp.LListener.offCaller(event, this);
-			}
-
-			super.destroy(isbool);
-		}
-
-		/**
-		 * 拉取界面信息
-		 */
-		public init_laqu(): void {
-			let pkt = new ProtoCmd.QuestClientData();
-			pkt.setString(this.talent)
-			lcp.send(pkt);
-		}
-		/**
-	  	 * 升级天赋
-	  	 * @param index 
-	  	 */
-		public lvUpTalent(): void {
-			let pkt = new ProtoCmd.QuestClientData();
-			pkt.setString(this.upLevelType)
-			lcp.send(pkt);
-		}
 		/**
 		 * 当前属性与星级
 		 * @param data 当前天赋
 		 */
 		public zhuangbeiInfo(data: ProtoCmd.ItemBase): void {
+			this.img_add.visible = this.img_battle.visible = true;
 			//天赋名称
-			this.lbl_name.text=SheetConfig.mydb_item_base_tbl.getInstance(null).ITEMNAME(''+data.dwBaseID);
-			this.img_type.skin = 'image/common/daoju/itemicon_'+data.dwBaseID+'.png';
+			this.lbl_name.text = SheetConfig.mydb_item_base_tbl.getInstance(null).ITEMNAME('' + data.dwBaseID);
+			this.img_type.skin = 'image/common/daoju/itemicon_' + data.dwBaseID + '.png';
 			//天赋等级
 			let talentAllData = GameApp.MainPlayer.talentInfo;
 			this.lbl_level.text = 'LV.' + talentAllData[this.dangqianNum];
@@ -235,7 +241,31 @@ module view.juese {
 				this.vbox_left.addChild(new view.juese.Person_attributeItem().setData(des[i], nextdes[i], 1))
 			}
 			//升到下阶战力增加
-			this.lbl_add.text=''+(nextBattle-battle);
+			this.lbl_add.text = '' + (nextBattle - battle);
+		}
+		/**
+	  * 拉取界面信息
+	  */
+		public init_laqu(): void {
+			let pkt = new ProtoCmd.QuestClientData();
+			pkt.setString(this.talent)
+			lcp.send(pkt);
+		}
+		/**
+	  	 * 升级天赋
+	  	 * @param index 
+	  	 */
+		public lvUpTalent(): void {
+			let pkt = new ProtoCmd.QuestClientData();
+			pkt.setString(this.upLevelType)
+			lcp.send(pkt);
+		}
+		public destroy(isbool): void {
+			for (let event of this.eventList) {
+				GameApp.LListener.offCaller(event, this);
+			}
+
+			super.destroy(isbool);
 		}
 	}
 }
