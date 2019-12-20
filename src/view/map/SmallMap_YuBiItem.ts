b910f84e653f1b38b15969179769230c8977b562
liyanmeng <799855844@qq.com>
2019-12-20 18:05:44 +0800

    地图

---
+++ src/view/map/SmallMap_YuBiItem.ts
@@ -0,0 +1,29 @@
+/**Created by the LayaAirIDE*/
+module view.map {
+	export class SmallMap_YuBiItem extends ui.map.SmallMap_YuBiItemUI {
+		constructor() {
+			super();
+		}
+		public setData(): void {
+			for (let i = 17001; i <= 17012; i++) {
+				let btn: Laya.Button = this['btn_' + i];
+				btn.label = '' + SheetConfig.mapRoomSheet.getInstance(null).ROOMNAME('' + i);
+				btn.labelSize = (btn.label.length > 3) ? 16 : 18;
+				btn.skin = 'image/map/smallMap/smallmap_icon_' + SheetConfig.mapRoomSheet.getInstance(null).ICONPIC('' + i) + '.png';
+			}
+			this.addEvent();
+		}
+		public addEvent(): void {
+			for (let i = 17001; i <= 17012; i++) {
+				EventManage.onWithEffect(this['btn_' + i], Laya.UIEvent.CLICK, this, () => {
+					// 隐藏自己
+					GameApp.SceneManager.showBigMap(false);
+					// 设置导航
+					let findMap = new GameUtil.findMapPath(17001, 17012).minPath(GameApp.MainPlayer.roomId, i);
+					console.log(findMap);
+					GameUtil.parseMapPath(findMap);
+				})
+			}
+		}
+	}
+}
