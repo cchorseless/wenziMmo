1e670952f2565be38e7cfa8389e1faf87f757b3b
ThomasYgz <519939537@qq.com>
2019-12-31 14:25:23 +0800

    小说

--- src/view/juQingMode/JuQing_MuLuInfo_item.ts
+++ src/view/juQingMode/JuQing_MuLuInfo_item.ts
@@ -4,15 +4,27 @@ module view.juQingMode {
 		constructor() {
 			super();
 		}
-		public setData(index, name, isLock, pid) {
+		public setData(index, name, isLock, pid, zjid) {
 			this.lab_charpterIndex.text = '第' + index + '章';
 			this.lab_charpterName.text = name;
 			if (isLock) {
+				this.img_bg.skin = 'image/juQingMode/box_ml_02down.png'
 				this.img_islock.visible = true;
-				this.img_showPage.visible = false;
+				this.lab_pageNum.visible = false;
 			} else {
+				if (zjid == GameApp.MainPlayer.charpterID) {
+					this.img_bg.skin = 'image/juQingMode/box_ml_02up.png'
+					this.lab_charpterIndex.color = '#4a6b88'
+					this.lab_charpterName.color = '#4a6b88'
+					this.lab_pageNum.color = '#4a6b88'
+				} else {
+					this.img_bg.skin = 'image/juQingMode/box_ml_02.png'
+					this.lab_charpterIndex.color = '#8c6240'
+					this.lab_charpterName.color = '#8c6240'
+					this.lab_pageNum.color = '#8c6240'
+				}
 				this.img_islock.visible = false;
-				this.img_showPage.visible = true;
+				this.lab_pageNum.visible = true;
 				let pages = 0;
 				let arr = GameApp.MainPlayer.pagesNum;
 				for (let i in GameApp.MainPlayer.pagesNum) {
