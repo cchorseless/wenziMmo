c5e0de73311a65e0ec63429877e08c50da1d1d98
liyanmeng <799855844@qq.com>
2019-12-31 10:28:41 +0800

    主界面

--- src/manage/PanelManage.ts
+++ src/manage/PanelManage.ts
@@ -203,7 +203,8 @@ module PanelManage {
             PanelManage.Main.updateUI();
             PanelManage.Main.view_scene.selectedIndex = 1;
             PanelManage.Main.box_menu.visible = true;
-            PanelManage.Main.box_mainBottom.visible=true;
+            PanelManage.Main.box_mainBottom.visible = true;
+            PanelManage.Main.box_mainTop.visible = true;
         }
         else {
             ResManage.loadResource(ResData.PanelRes.Main, () => {
@@ -211,8 +212,9 @@ module PanelManage {
                 PanelManage.Main['LCP_skin'] = ResData.PanelRes.Main;
                 PanelManage.Main.setData();
                 PanelManage.Main.mouseEnabled = true;
-                PanelManage.Main.box_mainBottom.visible=true;
+                PanelManage.Main.box_mainBottom.visible = true;
                 PopUpManager.addPanel(PanelManage.Main, 0);
+                PanelManage.Main.box_mainTop.visible = true;
             });
         }
     }
@@ -850,7 +852,8 @@ module PanelManage {
      */
     export function openJuQingModePanel(): void {
         PanelManage.openMainPanel();
-        PanelManage.Main.box_mainBottom.visible=false;
+        PanelManage.Main.box_mainTop.visible = false;
+        PanelManage.Main.box_mainBottom.visible = false;
         PanelManage.Main.box_menu.visible = false;
         if (PanelManage.Main.view_scene.numChildren == 2) {
             ResManage.loadResource(ResData.PanelRes.JuQingMode, () => {
